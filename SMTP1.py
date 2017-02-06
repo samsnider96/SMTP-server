05e6dbd7791a8dc0cedb0e247ce24323ac3945d8
Sam Snider <sam.snider301@gmail.com>
2017-02-06 11:34:11 -0500

    make first attempt at state machine for order of commands.  Fix some parsing errors

--- SMTP1.py
+++ SMTP1.py
@@ -1,6 +1,14 @@
 #This program will parse string commands and determine if they're valid SMTP commands.
 #validates the 'MAIL FROM', 'RCPT TO', and 'DATA' commands.
 
+#TO DO ::::  
+
+#FIX THE POST- '>' PARSING!!! 
+      #I think i've done this with a for loop... STILL NEED TO MAKE SURE THE LAST CHARS ARE /r/n
+          #Does \r\n count as one character or 2?
+# FIX THE a.b.c thing from the email...................
+#See if the 'MAIL ' or 'RCPT ' comparisons fuck up the tab case.  I think they would.
+
 import sys
 import string
 
@@ -22,7 +30,7 @@ def mFParser(s, l):  #s is the input string, l is the full input list.  l not cu
 
   mailStr = mailFromStr[:5]     #seperates off the 'mail' str, and checks it  
   if mailStr != 'MAIL ':
-    error500()
+    error500()              #I think this might fuck up the tab thing !!!!!
     return 0
 
   fromStr = mailFromStr[-4:]        #seperates off the 'from' str, and checks it
@@ -54,7 +62,7 @@ def mFParser(s, l):  #s is the input string, l is the full input list.  l not cu
   for y in pathL:
     if y == '<':    #this block checks the first path error
       break
-    if y != ' ':
+    if y != ' ' and y != '\t':
       error501()
       return 0
       break
@@ -131,13 +139,26 @@ def mFParser(s, l):  #s is the input string, l is the full input list.  l not cu
           #################################  final part of "mail from" #################################
 
   prePathClose, postPath = s.split('>', 1)    #postPaths is everything after the '>' character.
-  if postPath != '\r\n':
-    error501()
-    return 0
+  for b in postPath:
+    if b != '\r\n' and b != '\t' and b != ' ':
+      error501()
+      return 0        #Needa make sure this isn't an endless loop!
 
           #################################  End of MAIL FROM parsing #################################
 
   return 1
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -161,7 +182,7 @@ def rCParser(s, l):     #Parses the RCPT-TO string.
 
   rcptStr = rcptToStr[:5]     #seperates off the 'RCPT' str, and checks it  
   if rcptStr != 'RCPT ':
-    error500()
+    error500()            #I think this might fuck up the tab thing !!!!!
     return 0
 
   toStr = rcptToStr[-2:]        #seperates off the 'TO' str, and checks it
@@ -190,7 +211,7 @@ def rCParser(s, l):     #Parses the RCPT-TO string.
   for y in pathL:
     if y == '<':    #this block checks the first path error
       break
-    if y != ' ':
+    if y != ' ' and y != '\t':
       error501()
       return 0
       break
@@ -267,13 +288,55 @@ def rCParser(s, l):     #Parses the RCPT-TO string.
           #################################  final part of "rcpt-to-cmd" #################################
 
   prePathClose, postPath = s.split('>', 1)    #postPaths is everything after the '>' character.
-  if postPath != '\r\n':
-    error501()
-    return 0
+
+  for b in postPath:
+    if b != '\r\n' and b != '\t' and b != ' ':
+      error501()
+      return 0        #Needa make sure this isn't an endless loop!
 
           #################################  End of RCPT-TO parsing #################################
 
   return 1
+
+
+
+
+
+
+
+def dataChecker(s, l):  #data cheker is the same as data parser, but simply doesn't print error messages.
+
+  dataStr = s[:4]     #seperates off the 'DATA' str, and checks it  
+  if mailStr != 'DATA':
+    return 0
+
+  preA, postA = s.split('A', 2)    #postPaths is everything after the second 'A' character.
+
+  for i in postA:
+    if i != '\r\n' and i != '\t' and i != ' ':
+      return 0        #Needa make sure this isn't an endless loop!
+
+  return 1
+
+
+def dataParser(s, l):
+
+  dataStr = s[:4]     #seperates off the 'DATA' str, and checks it  
+  if mailStr != 'DATA':
+    error500()
+    return 0
+
+  preA, postA = s.split('A', 2)    #postPaths is everything after the second 'A' character.
+
+  for i in postA:
+    if i != '\r\n' and i != '\t' and i != ' ':
+      error500()
+      return 0        #Needa make sure this isn't an endless loop!
+
+  return 1
+
+
+
 
 
 def error500(): 
@@ -294,20 +357,44 @@ def error503():
 
 def main():
 
+                        #set state machine variables:
+  stateCheckerMF = 0
+  stateCheckerRC = 0
+  oneOrMoreRc = 0  #checks if there's been one or more valid RCPT TO commands
+
   try:
     while 1:                #accept input, parse it, and provide output in a loop.
       
-      inVarMF = raw_input() + '\r\n'    #mail from parser call
-      inListMF = list(inVarMF)
-      print inVarMF[0:inVarMF.index('\r')]
-      if mFParser(inVarMF, inListMF) == 1:
-        print '250 OK'
 
-      inVarRC = raw_input() + '\r\n'     #rcpt-to parser call
-      inListRC = list(inVarRC)
-      print inVarRC[0:inVarRC.index('\r')]
-      if rCParser(inVarRC, inListRC) == 1:
-        print '250 OK'
+      while stateCheckerMF == 0:
+        inVarMF = raw_input() + '\r\n'    #mail from parser call
+        inListMF = list(inVarMF)
+        print inVarMF[0:inVarMF.index('\r')]
+        if mFParser(inVarMF, inListMF) == 1:
+          print '250 OK'
+          stateCheckerMF == 1
+
+
+      while stateCheckerRC == 0:
+        inVarRC = raw_input() + '\r\n'     #rcpt-to parser call
+        inListRC = list(inVarRC)
+
+        if dataChecker(inVarRC, inListRC) == 1 and oneOrMoreRc == 1    
+          stateCheckerRC = 1                      
+          break                                 #Checks that the state machine shouldn't move on.
+
+        print inVarRC[0:inVarRC.index('\r')]
+          if rCParser(inVarRC, inListRC) == 1:
+            moreThanOneRc = 1     #setting this variable means that >=1 valid RCPT TO command has been read.
+            print '250 OK'
+
+
+      inVarData = raw_input() + '\r\n'
+      inListData = list(inVarData)
+      if dataParser(inVarData, inListData) == 1:
+        print '354 Start mail input; end with <CRLF>.<CRLF>'
+
+
   except EOFError:
     pass
 
